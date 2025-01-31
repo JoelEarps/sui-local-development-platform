91d2c31ed270d83ccd1f2249a3f055077bb46d1b
Joel Earps <joel.earps@swissborg.com>
2025-01-31 10:39:29 +0000

    feat: DeepBook OB to Generic OB implementation

--- rust_sui_connector/src/errors/data_fetching_errors.rs
+++ rust_sui_connector/src/errors/data_fetching_errors.rs
@@ -1,3 +1,4 @@
+use bigdecimal::ParseBigDecimalError;
 use thiserror::Error;
 
 
@@ -6,9 +7,12 @@ pub enum DataFetchingErrors {
     #[error("Failed to fetch the data from the API")]
     FetchError(#[from] reqwest::Error),
 
-    // #[error("Failed to fetch the data from the API")]
-    // UrlParseError(#[from] reqwest::Error),
-    
+    #[error("Failed to convert orderbook data to require format")]
+    DeepBookV3DeserialisationError(),
+
+    #[error("Failed to parse bid and quantity")]
+    BigDecimalParsingError(#[from] ParseBigDecimalError),
+
     #[error("Retry limit exceeded ({0} retries)")]
     RetryError(u8),
 }
--- rust_sui_connector/src/market_data_collector/request_handler.rs
+++ rust_sui_connector/src/market_data_collector/request_handler.rs
@@ -4,50 +4,74 @@ use std::io::Error;
 use std::io::ErrorKind;
 use anyhow::anyhow;
 use anyhow::Result;
+use connector_model::orderbook::PriceLevel;
+use connector_model::pricing::Quantity;
+use connector_model::pricing::Rate;
 use serde::Deserialize;
 use crate::errors::data_fetching_errors::DataFetchingErrors;
 use connector_model::orderbook::OrderBook;
-
-// TODO: Look at Mocking once initial functionality is complete
-// use mockall::predicate::*;
-// use mockall::automock;
-// use mockall_double::double;
-
-
-// #[derive(Clone)]
-// pub(crate) struct RequestHandler{
-//     base_url: String
-// }
-
-// #[automock]
-// pub trait MakeRequest{
-//     async fn make_request(&self) -> Result<bool, ()>;
-// }
-
-// impl MakeRequest for RequestHandler {
-//     async fn make_request(&self) -> Result<bool, ()> {
-//         // Look at removing this, just getting it working for now
-//         // get(self.base_url.clone());
-//         println!("Making Request to DeepBook Indexer");
-//         Ok(true) 
-//     }
-// }
-
-// impl RequestHandler {
-//     pub fn new(base_url_from_config: String) -> Self {
-//         Self {
-//             base_url: base_url_from_config
-//         }
-//     }
-// }
+use std::str::FromStr;
+use bigdecimal::BigDecimal;
 
 #[derive(Deserialize, Debug)]
-pub struct DeepBookOrderBookData {
+pub struct DeepBookV3OrderBookData {
     timestamp: String,
-    bids: Vec<Vec<String>>,
-    asks: Vec<Vec<String>>
+    bids: Vec<DeepBookV3PriceLevel>,
+    asks: Vec<DeepBookV3PriceLevel>
 }
 
+#[derive(Deserialize, Debug)]
+#[serde(try_from = "Vec<String>")]
+struct DeepBookV3PriceLevel {
+    price: BigDecimal,
+    quantity: BigDecimal
+}
+
+impl TryFrom<Vec<String>> for DeepBookV3PriceLevel {
+    type Error = DataFetchingErrors;
+    fn try_from(value: Vec<String>) -> std::result::Result<Self, Self::Error> {
+        if value.len() == 2 {
+            Ok(DeepBookV3PriceLevel {
+                price: BigDecimal::from_str(&value[0])?,
+                quantity: BigDecimal::from_str(&value[1])?
+            })
+        } else {
+            Err(DataFetchingErrors::DeepBookV3DeserialisationError())
+        } 
+    }
+}
+
+impl From<DeepBookV3PriceLevel> for PriceLevel {
+    fn from(value: DeepBookV3PriceLevel) -> Self {
+        PriceLevel {
+            price: Rate::new(value.price),
+            quantity: Quantity::new(value.quantity)
+        }
+    }
+}
+
+impl From<DeepBookV3OrderBookData> for OrderBook {
+    fn from(value: DeepBookV3OrderBookData) -> Self {
+
+        let ask_price_levels = value.asks
+        .into_iter()
+        .map(| data | PriceLevel::from(data))
+        .collect();
+
+        let bid_price_levels= value.bids.into_iter().
+        map(|data| PriceLevel::from(data))
+        .collect();
+
+        OrderBook::new(bid_price_levels, ask_price_levels)
+    }
+}
+
+// Convert from DeepBook -> Generic OrderBook
+// Make this a struct that implements the trait MarketBuilder
+// Get head round difference between Market and current function
+
+// OrderBook -> Vector of bids and ask of type PriceLevel, PriceLevel has two members, price and amount of type Rate and Quantity respectively.
+// Therefore we need a way of converting to Price Levels to construct new OrderBook data.
 pub(crate) async fn fetch_market_data_at_required_rate(base_url_from_config: String, rate: u64) -> Result<(), DataFetchingErrors> {
     println!("Fetching market data from DeepBook Indexer at a rate of {rate} seconds");
     let mut attempt_counter = 0;
@@ -56,7 +80,10 @@ pub(crate) async fn fetch_market_data_at_required_rate(base_url_from_config: Str
         // Pop this on a mutex to see if it has been changed? Clever way of checking if this has changed?
         let test = make_request(base_url_from_config.clone(), "SUI_USDC", 4).await;
         match test {
-            Ok(_) => println!("{test:#?}"),
+            Ok(deepbook_order_book_data) => {
+                let generic_orderbook = OrderBook::from(deepbook_order_book_data);
+                println!("{:#?}", generic_orderbook);
+            },
             Err(_) => attempt_counter+=1
         }
         tokio::time::sleep(tokio::time::Duration::from_secs(rate)).await;
@@ -67,12 +94,12 @@ pub(crate) async fn fetch_market_data_at_required_rate(base_url_from_config: Str
     }
 }
 
-pub(super) async fn make_request(base_url: String, market_id: &str, depth: u8) -> Result<DeepBookOrderBookData, DataFetchingErrors> {
+pub(super) async fn make_request(base_url: String, market_id: &str, depth: u8) -> Result<DeepBookV3OrderBookData, DataFetchingErrors> {
     let url_with_params = format!("{}{}?level={}&depth={}",base_url, market_id, 2, depth);
     println!("Making request to {url_with_params}");
     let resp = reqwest::get(url_with_params)
         .await?
-        .json::<DeepBookOrderBookData>()
+        .json::<DeepBookV3OrderBookData>()
         .await?;
     println!("{:?}", resp);
     Ok(resp)
